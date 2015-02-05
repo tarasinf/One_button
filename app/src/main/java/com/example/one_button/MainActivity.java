c213e5987fc2ab9c11a42b5616505fa16b1a3858
pseverin <severyn.parkhomenko@sannacode.com>
2015-02-05 12:48:12 +0200

    seva commit

--- app/src/main/java/com/example/one_button/MainActivity.java
+++ app/src/main/java/com/example/one_button/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
+/*Це Сєва і я редагую твій проект*/
 public class MainActivity extends ActionBarActivity {
 
     private EditText editText;
